4209b3d21947b54e52b96dcdf2f78a843a97b354
cdkkyj123 <cdkkyj123@gmail.com>
2026-03-11 14:27:46 +0900

    refactor : local test refactor

--- src/test/java/com/example/teamintroservice/TeamIntroServiceApplicationTests.java
+++ src/test/java/com/example/teamintroservice/TeamIntroServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.teamintroservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.config.import=optional:aws-parameterstore:")
 class TeamIntroServiceApplicationTests {
 
     @Test
