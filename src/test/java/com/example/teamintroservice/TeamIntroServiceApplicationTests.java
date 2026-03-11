791461797a63ec5c4bc39cf468d7b555c410ec15
cdkkyj123 <cdkkyj123@gmail.com>
2026-03-11 14:42:18 +0900

    refactor : run test refactor

--- src/test/java/com/example/teamintroservice/TeamIntroServiceApplicationTests.java
+++ src/test/java/com/example/teamintroservice/TeamIntroServiceApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.teamintroservice;
 
+import io.awspring.cloud.s3.S3Template;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest(properties = "spring.config.import=optional:aws-parameterstore:")
 class TeamIntroServiceApplicationTests {
 
+    @MockBean
+    private S3Template s3Template;
+
     @Test
     void contextLoads() {
     }
-
 }
