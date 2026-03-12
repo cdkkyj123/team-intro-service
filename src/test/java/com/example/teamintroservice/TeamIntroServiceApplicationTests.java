18441f6f14b8aa3df5680a138543fadb7fed3ffe
cdkkyj123 <cdkkyj123@gmail.com>
2026-03-12 22:29:30 +0900

    fix : prod ddl-auto set validate

--- src/test/java/com/example/teamintroservice/TeamIntroServiceApplicationTests.java
+++ src/test/java/com/example/teamintroservice/TeamIntroServiceApplicationTests.java
@@ -4,11 +4,13 @@ import io.awspring.cloud.s3.S3Template;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(properties = {
         "spring.config.import=optional:aws-parameterstore:",
         "spring.cloud.aws.cloudfront.domain=https://dummy.cloudfront.net"
 })
+@ActiveProfiles("local")
 class TeamIntroServiceApplicationTests {
 
     @MockBean
